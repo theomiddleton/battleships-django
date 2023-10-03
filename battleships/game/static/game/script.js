113719641918f9cbd217e3cde031b48e41c68f08
theo <59509832+theomiddleton@users.noreply.github.com>
2023-10-03 09:45:57 +0000

    made end of game + restart button

--- battleships/game/static/game/script.js
+++ battleships/game/static/game/script.js
@@ -1,5 +1,7 @@
 const restartButton = document.getElementById('restart-button');
+const cells = document.querySelectorAll('.cell');
 const randomCells = document.querySelectorAll('.random');
+let foundRandomCells = 0;
 
 function showRestartButton() {
     const foundRandomCells = document.querySelectorAll('.random.found');
@@ -13,27 +15,32 @@ function showRestartButton() {
         }
     }
 }
-document.querySelectorAll('.cell').forEach(cell => {
+
+cells.forEach(cell => {
     cell.addEventListener('click', function() {
+        if (foundRandomCells === randomCells.length) {
+            return;
+        }
         const idParts = this.id.split('_');
         const row = idParts[1];
         const column = idParts[2];
         const is_random = this.classList.contains('random');
+        //fetch(`/clicked/${row}/${column}/${isRandom}/`)
         const is_random_int = is_random ? 1 : 0;
         fetch(`/clicked/${row}/${column}/${is_random_int}/`)
             .then(response => response.text())
             .then(data => {
                 console.log(data);
-                showRestartButton();
+                if (is_random) {
+                    this.style.backgroundColor = 'green';
+                    const message = document.getElementById('message');
+                    message.textContent = 'You hit a ship!';
+                    this.classList.add('found');
+                    foundRandomCells++;
+                    showRestartButton();
+                } else {
+                    this.style.backgroundColor = 'red';
+                }
             });
-        if (is_random) {
-            this.style.backgroundColor = 'green';
-            const message = document.getElementById('message');
-            message.textContent = 'You hit a ship!';
-            this.classList.add('found');
-            showRestartButton();
-        } else {
-            this.style.backgroundColor = 'red';
-        }
     });
 });
