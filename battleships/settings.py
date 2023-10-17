4dd4eed4c2cc75d75854ea2f5110d7b4b3dd2091
theo <theo.middleton@outlook.com>
2023-10-17 15:36:02 +0100

    moved files out of unnecessary folder

--- battleships/settings.py
+++ battleships/settings.py
@@ -23,7 +23,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-4)p1auctjyb+o)20q%mu&g8ji^&!@ijh@*9zjz46axl8kxuq-1"
+
+SECRET_KEY = os.environ.get('Django_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
